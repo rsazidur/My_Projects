bc75e253c04836d0d01d079dcb248a90041528c0
Molla Sazidur Rahman <rsazidur936@gmail.com>
2022-05-19 15:15:14 +0600

    Creating a Setting Class

---
+++ Project 2/settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """A class to store all settings for alien invasion."""
+    def __init__(self):
+        """Initialize the games settings."""
+        # Screen settings
+        self.screen_width = 1920
+        self.screen_height = 1080
+        self.bg_color = (230, 230, 230)
