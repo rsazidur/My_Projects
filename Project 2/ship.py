9a32b14bbc6a5c27f2de1d5bfa759681eb716f4a
Molla Sazidur Rahman <rsazidur936@gmail.com>
2022-05-20 23:58:33 +0600

    Updated left

--- Project 2/ship.py
+++ Project 2/ship.py
@@ -28,7 +28,7 @@ class Ship:
         # Update the ship's x value not the rect.
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left and self.rect > 0:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
 
         # Update rect object from self.x
@@ -37,4 +37,3 @@ class Ship:
     def blitme(self):
         """Draw the ship at its current location"""
         self.screen.blit(self.image, self.rect)
-
